12391e48210b751855afbe55f2bae32cf186f7a2
Riki <rick.libby.91@gmail.com>
2017-08-01 16:30:47 +0000

    add employee passwords and generate before create

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,7 +1,19 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates :first_name, 
+            :last_name, 
+              presence: true
 
+  validates :hourly_rate,
+              numericality: { 
+                only_integer: true, 
+                greater_than_or_equal_to: 40, 
+                less_than_or_equal_to: 200 
+              }
+
+  before_create :create_random_password
+
+  def create_random_password
+    self.password = rand(36**11).to_s(36).upcase[0,8]
+  end
 end
